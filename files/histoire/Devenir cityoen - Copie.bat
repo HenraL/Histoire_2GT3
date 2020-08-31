4e25c024c032a58b250bc0c8618b4bf2bb8ba323
HenraL <37299738+HenraL@users.noreply.github.com>
2020-08-31 15:34:36 +0200

    uploaded the files

---
+++ files/histoire/Devenir cityoen - Copie.bat
@@ -0,0 +1,35 @@
+color 0A
+cls
+echo off
+cls
+echo " _     _   _______        ________    _______    _______   ____    ______"
+echo "| |   | | |__   __|      |___  ___|  /  ___  \  |__   __| | __ \  |  ____|"
+echo "| |___| |    | |     ____    | |    /  /   \  \    | |    ||__) | | |____"
+echo "|  ___  |    | |    /  __|   | |    | |     | |    | |    |    /  |  ____|"
+echo "| |   | |  __| |__  \__  \   | |    \  \___/  /  __| |__  | |\ \  | |____"
+echo "|_|   |_| |_______| |____/   |_|     \_______/  |_______| |_| \_\ |______|"
+echo ""
+echo "								/"
+echo "							       ____    ___"
+echo "  							/     |____   / _ \"
+echo "							| __  |____  | |_| |"
+echo "							\__/          \___/"
+echo .
+echo .
+echo .
+echo 29/09/2017
+echo sceance 1: une democratie
+echo .
+echo Aristote est un athénien grec qui a laisser une trace sur l'idée de la constitution d'Athenes
+echo .
+echo doc 1 p 42:
+echo .
+echo Devenir citoyen
+echo .
+echo Les enfants nes de parents qui ont tous deux droit de cite les enfants prennent part eux aussi a la cite. Les jeunes sont inscrit au nombre des habitants d'un dème à l'âge de dix-huit ans. à cette inscription les habitants du deme, apres avoir fait leur serment ils décident par un vote.(seulement s'ils ont l'age exiger par la loi et s'ils sont de condition libre et de naissance legitime.)
+echo .
+echo il y a la tournée dans les sanctuaires: initiation religieuse
+echo .
+echo il vont au piree: le port d'Athene(militaire)
+pause
+pause
